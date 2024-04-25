ffe935d476d4323606e60614b048e2387450800f
Daniel Johansson <dannesnake@gmail.com>
2024-04-25 15:32:51 +0200

    Add class Interest to models A model for table Interests. Should have a one to many relationship with Person table

---
+++ Labb3Models/Interest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3Models
+{
+    internal class Interest
+    {
+        [Key]
+        public int InterestId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Person Person { get; set; }
+    }
+}
