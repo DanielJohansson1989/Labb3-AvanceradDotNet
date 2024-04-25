2cc57997b1b404eb649aaf5fa68a1d0312b39768
Daniel Johansson <dannesnake@gmail.com>
2024-04-25 15:23:28 +0200

    Create class library A class library that will contain necessary models for the database. First model Person is created.

---
+++ Labb3Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labb3Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        public string? Phone { get; set; }
+    }
+}
