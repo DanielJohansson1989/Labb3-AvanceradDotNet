88b184e1aba48f0da8cee591ddaf0d31974e58e4
Daniel Johansson <dannesnake@gmail.com>
2024-04-25 17:41:16 +0200

    Create Interface for CRUD operations

---
+++ Labb3-AvanceradDotNet/Services/ILabb3.cs
@@ -0,0 +1,11 @@
+﻿namespace Labb3_AvanceradDotNet.Services
+{
+    public interface ILabb3<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Add(T newEntity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
+    }
+}
